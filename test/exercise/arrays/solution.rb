4f5e2ac704bb48fd038db006f6a08cb719954142
semensid2020 <semensid2020@gmail.com>
2023-07-07 19:07:33 +0700

    Arrays tasks completed

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,12 +2,35 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array
+        max = array[0]
+        array.each do |i|
+          max = i if max < i
+        end
+
+        array.map! do |i|
+          i.positive? ? max : i
+        end
       end
 
       def search(_array, _query)
-        0
+        low = 0
+        high = _array.size - 1
+
+        while low <= high
+          mid = (low + high).div(2)
+          mid_value = _array[mid]
+          if mid_value == _query
+            return mid
+          elsif mid_value > _query
+            high = mid - 1
+          else
+            low = mid + 1
+          end
+        end
+
+        return -1
       end
+
     end
   end
 end
