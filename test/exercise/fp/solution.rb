4655a15bcaf9f490b1109c919dc28f237eec55bb
semensid2020 <semensid2020@gmail.com>
2023-07-08 12:33:54 +0700

    FP rating task completed

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -4,8 +4,19 @@ module Exercise
       # Обратиться к параметрам фильма можно так:
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
+      def countries_number(record)
+        record["country"]&.split(',')&.count || 0
+      end
+
+      def accounted_in_rating?(record)
+        record["rating_kinopoisk"].to_f.positive? && countries_number(record) > 1
+      end
+
       def rating(_array)
-        0
+        ratings_filtered = _array.select{ |f| accounted_in_rating?(f) }
+                                 .map{ |f| f["rating_kinopoisk"].to_f }
+
+        ratings_filtered.reduce(:+) / ratings_filtered.size
       end
 
       def chars_count(_films, _threshold)
