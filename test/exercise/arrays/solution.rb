1d509aefce41c1d891b7687667df6fb2d18b68af
semensid2020 <semensid2020@gmail.com>
2023-07-08 13:12:01 +0700

    Arrays tasks refactored

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,14 +2,18 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        max = array[0]
-        array.each do |i|
-          max = i if max < i
-        end
+        max = find_max(array)
 
         array.map! do |i|
           i.positive? ? max : i
         end
+      end
+
+      def find_max(arr)
+        max = arr[0]
+        arr.each { |i| max = i if max < i }
+
+        max
       end
 
       def search(_array, _query)
