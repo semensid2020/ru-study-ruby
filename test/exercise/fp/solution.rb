eb4348b5b30284f9158e0d5b3cf31cbeb065c7a8
semensid2020 <semensid2020@gmail.com>
2023-07-11 21:40:46 +0700

    FP chars counting task: rubocop satisfied

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -5,24 +5,24 @@ module Exercise
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
       def countries_number(record)
-        record["country"]&.split(',')&.count || 0
+        record['country']&.split(',')&.count || 0
       end
 
       def accounted_in_rating?(record)
-        record["rating_kinopoisk"].to_f.positive? && countries_number(record) > 1
+        record['rating_kinopoisk'].to_f.positive? && countries_number(record) > 1
       end
 
       def rating(array)
-        ratings_filtered = array.select{ |f| accounted_in_rating?(f) }
-                                 .map{ |f| f["rating_kinopoisk"].to_f }
+        ratings_filtered = array.select { |f| accounted_in_rating?(f) }
+                                .map { |f| f['rating_kinopoisk'].to_f }
 
         ratings_filtered.reduce(:+) / ratings_filtered.size
       end
 
       def chars_count(films, threshold)
-        films.select{ |f| f["rating_kinopoisk"].to_f >= threshold }
-              .map{ |f| f["name"].count('и') }
-              .reduce(:+)
+        films.select { |f| f['rating_kinopoisk'].to_f >= threshold }
+             .map { |f| f['name'].count('и') }
+             .reduce(:+)
       end
     end
   end
