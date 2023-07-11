932d9b988d8b579449c39a00bf972b06337fd385
semensid2020 <semensid2020@gmail.com>
2023-07-11 21:34:01 +0700

    Arrays tasks: rubocop satisfied

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -18,15 +18,14 @@ module Exercise
         size = array.size
         half_index = size.div(2)
 
-        return -1 if (size == 0 || array[-1] < query || array[0] > query)
+        return -1 if size.zero? || array[-1] < query || array[0] > query
         return shift if array[0] == query
         return (shift + size - 1) if array[-1] == query
         return (shift + half_index) if array[half_index] == query
 
         return search(array[0..(half_index - 1)], query, shift) if array[half_index] > query
-        return search(array[(half_index + 1)..-1], query, shift + half_index + 1) if array[half_index] < query
+        return search(array[(half_index + 1)..], query, shift + half_index + 1) if array[half_index] < query
       end
-
     end
   end
 end
