2385c538544aaf818f054e15c1b4545c2867129f
semensid2020 <semensid2020@gmail.com>
2023-07-16 10:02:56 +0700

    Arrays tasks: refactored once again

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -14,21 +14,16 @@ module Exercise
         max
       end
 
-      def search(array, query, shift = 0)
-        # shift - то, насколько "единиц" смещён переданный методу массив относительно начала исходного
-        size = array.size
-        half_index = size.div(2)
+      def search(array, query, left = 0, right = array.size - 1)
+        if right >= left
+          half_index = left + (right - left) / 2
+          return half_index if array[half_index] == query
 
-        # Смотрим граничные значения
-        return -1 if size.zero? || array[-1] < query || array[0] > query
-        return shift if array[0] == query
-        return (shift + size - 1) if array[-1] == query
-        return (shift + half_index) if array[half_index] == query
+          return search(array, query, left, half_index - 1) if array[half_index] > query
 
-        # Идем в левую половину если запрашиваемое значение меньше чем "серединный элемент":
-        return search(array[0..(half_index - 1)], query, shift) if array[half_index] > query
-        # Идем в правую половину если запрашиваемое значение больше чем "серединный элемент":
-        return search(array[(half_index + 1)..], query, shift + half_index + 1) if array[half_index] < query
+          return search(array, query, half_index + 1, right)
+        end
+        -1
       end
     end
   end
